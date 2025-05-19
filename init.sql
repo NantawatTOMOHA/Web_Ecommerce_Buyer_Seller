94da2d4a3b9031bc463f2632506635bf62f7abdc
Nantawat Rattanaruangviman <69116458+NantawatTOMOHA@users.noreply.github.com>
2025-05-19 14:54:35 +0700

    update
    update

---
+++ init.sql
@@ -0,0 +1,61 @@
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
+);
+INSERT INTO roles (name) VALUES ('buyer'), ('seller');
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  full_name VARCHAR(100) NOT NULL,
+  role_id INT REFERENCES roles(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  price NUMERIC(10, 2) NOT NULL,
+  seller_id INT REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  quantity INTEGER NOT NULL,
+  image BYTEA,
+  status BOOLEAN DEFAULT TRUE,
+  is_deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  buyer_id INTEGER REFERENCES users(id),
+  product_id INTEGER REFERENCES products(id),
+  order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  total_amount NUMERIC(10,2) NOT NULL,
+  status VARCHAR(50) DEFAULT 'Pending',
+  address TEXT NOT NULL
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY,
+  order_id INTEGER REFERENCES orders(id),
+  product_id INTEGER REFERENCES products(id),
+  quantity INTEGER NOT NULL,
+  unit_price NUMERIC(10,2) NOT NULL,
+  total_price NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE carts (
+  id SERIAL PRIMARY KEY,
+  buyer_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cart_items (
+  id SERIAL PRIMARY KEY,
+  cart_id INTEGER REFERENCES carts(id),
+  product_id INTEGER REFERENCES products(id),
+  quantity INTEGER NOT NULL,
+  unit_price NUMERIC(10,2) NOT NULL,
+  total_price NUMERIC(10,2) NOT NULL
+);
